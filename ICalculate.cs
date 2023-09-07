91810daa93df0b7360482b4f97a93e74f255ea55
SanaHejazi <sanahejazirad@gmail.com>
2023-09-07 11:35:47 +0330

    adding buttons and interface ...

---
+++ ICalculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	internal interface ICalculate
+	{
+		int plus(int numberone, int numbertow);
+		int mine(int numberone, int numbertow);
+		int div(int numberone, int numbertow);
+		int mult(int numberone, int numbertow);
+	}
+}
